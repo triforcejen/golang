4af8459ea6b6bbc9c785a5367cf46d3ac0ccb0dc
triforcejen <triforcejen@gmail.com>
2021-12-03 15:00:14 +0000

    Basic solution to exercise.

--- exercise-loops-and-functions/exercise-loops-and-functions.go
+++ exercise-loops-and-functions/exercise-loops-and-functions.go
@@ -5,9 +5,19 @@ import (
 )
 
 func Sqrt(x float64) float64 {
-
+	//Declare and initialize a floating point value
+	z := float64(1)
+	fmt.Printf("Calculating square root for: %v\n", x)
+	//Repeat the calculation ten times
+	for i := 0; i < 10; i++ {
+		
+		//Square root calculation
+		z -= (z*z - x) / (2*z)
+		fmt.Printf("Round %v: Square root is: %v\n", i, z)
+	}
+	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(4))
 }
