900fbe84f915833594c566078b6aedf906d4762e
triforcejen <triforcejen@gmail.com>
2021-12-03 17:30:29 +0000

    End loop when Square root is calculated to be the same number twice in a row.

--- exercise-loops-and-functions/exercise-loops-and-functions.go
+++ exercise-loops-and-functions/exercise-loops-and-functions.go
@@ -5,20 +5,30 @@ import (
 )
 
 func Sqrt(x float64) float64 {
-	//Declare and initialize a floating point value
+	
+	//Declare and initialize a floating point value.
 	z := float64(1)
-	fmt.Printf("Calculating square root for: %v\n", x)
-	//Repeat the calculation ten times
-	for i := 0; i < 10; i++ {
+	old_Z := float64(0) 
+	
 		
-		//Square root calculation
+	fmt.Printf("Calculating square root for: %v\n", x)
+	
+	//Repeat the calculation ten times.
+	//Loop condition to stop once the value has stopped changing.
+	for i := 0; i < 10 && z != old_Z; i++ {
+		old_Z = z
+		
+		//Square root calculation.
 		z -= (z*z - x) / (2*z)
-		fmt.Printf("Round %v: Square root is: %v\n", i, z)
+		
+		fmt.Printf("Round %v: Square root is: %v\n\n", i, z)
+		
 	}
 	return z
 }
 
 func main() {
+	//See how close the answer is for various values.
 	for i := 0; i <10; i++ {
 		fmt.Println(Sqrt(float64(i)))
 	}
